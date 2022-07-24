545d0acd832cc516134df53f6822862e59e29bcd
VasylKhrapko <khrapko2002@gmail.com>
2022-07-24 13:10:45 +0300

    Added dotenv dependency, and dotenv configuration file

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 'use strict';
 
+require('dotenv').config();
 const app = require('./app');
-const port = 8080;
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+const PORT = process.env.PORT || 8080;
+const HOST = process.env.HOST || 'localhost';
+
+app.listen(PORT, HOST, () => {
+  console.log(`App listening on http://${HOST}:${PORT}`);
 });
