fb01ea456aa4cc326fd88b0e7a0dfd01751be25b
VasylKhrapko <khrapko2002@gmail.com>
2022-07-26 11:19:41 +0300

    Added tests for File-Based Database class

---
+++ src/db/fileBasedDB.test.js
@@ -0,0 +1,154 @@
+/* eslint-disable max-len */
+'use strict';
+
+const path = require('path');
+const fsp = require('fs').promises;
+const FileBasedDB = require('./fileBasedDB');
+
+describe('File Based Database Testing', () => {
+  const dirPath = path.join(__dirname, 'testData');
+  const emailsFilePath = path.join(dirPath, 'emails.txt');
+  const db = new FileBasedDB(dirPath);
+
+  beforeEach(async () => {
+    await db.connect();
+  });
+
+  afterEach(async () => {
+    await db.clearDB();
+    await fsp
+      .rm(dirPath, { recursive: true, force: true })
+      .catch(err => console.log(err));
+  });
+
+  describe('Testing the .connect() method', () => {
+    // The .connect() method creates
+    // a directory at the specified path (and database files in this directory).
+    // We don't call it directly in the test
+    // because this method is called before every test.
+
+    test('Should create directory ./test and not throw an error when trying to access this folder', async () => {
+      const testFolderAccess = async () => await fsp.access(dirPath);
+
+      expect(testFolderAccess).not.toThrow();
+    });
+
+    test('Should create datafile ./test/emails.txt and not throw an error when trying to access this datafile', async () => {
+      const testFileAccess = async () => await fsp.access(emailsFilePath);
+
+      expect(testFileAccess).not.toThrow();
+    });
+  });
+
+  describe('Testing the .insertEmail() method', () => {
+    test(`Should insert a new email address into the database (which was not previously in the database),
+    the return value should be equal to the email being inserted`, async () => {
+      const newEmail = 'test@gmail.com';
+
+      const returnedEmail = await db.insertEmail(newEmail);
+
+      const dataInEmailsFile = await fsp.readFile(emailsFilePath, {
+        encoding: 'utf-8',
+      });
+      const emailsArr = dataInEmailsFile.trim().split('\n');
+      const insertedEmail = emailsArr[0];
+
+      expect(returnedEmail).toBe(newEmail);
+      expect(insertedEmail).toBe(newEmail);
+      expect(emailsArr).toHaveLength(1);
+    });
+
+    test('Should throw an error when trying to add an email that has already been recorded in the database', async () => {
+      const newEmail = 'test@gmail.com';
+
+      try {
+        expect.assertions(3);
+        await db.insertEmail(newEmail);
+
+        await db.insertEmail(newEmail);
+      } catch (err) {
+        expect(err).not.toBe(null);
+        expect(err).not.toBe(undefined);
+        expect(err.message).toBe(
+          'The specified email has already been recorded'
+        );
+      }
+    });
+
+    test('Should successfully add three unique emails to the database', async () => {
+      const newEmail1 = 'test1@gmail.com';
+      const newEmail2 = 'test2@gmail.com';
+      const newEmail3 = 'test3@gmail.com';
+
+      const returnedEmail1 = await db.insertEmail(newEmail1);
+      const returnedEmail2 = await db.insertEmail(newEmail2);
+      const returnedEmail3 = await db.insertEmail(newEmail3);
+
+      const dataInEmailsFile = await fsp.readFile(emailsFilePath, {
+        encoding: 'utf-8',
+      });
+      const emailsArr = dataInEmailsFile.trim().split('\n');
+
+      expect(returnedEmail1).toBe(newEmail1);
+      expect(returnedEmail2).toBe(newEmail2);
+      expect(returnedEmail3).toBe(newEmail3);
+      expect(emailsArr).toHaveLength(3);
+      expect(emailsArr).toContain(newEmail1);
+      expect(emailsArr).toContain(newEmail2);
+      expect(emailsArr).toContain(newEmail3);
+    });
+  });
+
+  describe('Testing the .findAllEmails() method', () => {
+    test(`Should return an array containing the 3 emails added earlier to the database`, async () => {
+      const emailToAdd1 = 'test1@gmail.com';
+      const emailToAdd2 = 'test2@gmail.com';
+      const emailToAdd3 = 'test3@gmail.com';
+      await db.insertEmail(emailToAdd1);
+      await db.insertEmail(emailToAdd2);
+      await db.insertEmail(emailToAdd3);
+
+      const emailsArr = await db.findAllEmails();
+
+      expect(Array.isArray(emailsArr)).toBe(true);
+      expect(emailsArr).toHaveLength(3);
+      expect(emailsArr).toContain(emailToAdd1);
+      expect(emailsArr).toContain(emailToAdd2);
+      expect(emailsArr).toContain(emailToAdd3);
+    });
+
+    test(`Should return an empty array if no email has been inserted`, async () => {
+      const emailsArr = await db.findAllEmails();
+
+      expect(Array.isArray(emailsArr)).toBe(true);
+      expect(emailsArr).toHaveLength(0);
+    });
+  });
+
+  describe('Testing the .clearDB() method', () => {
+    test(`Should clear all inserted email addresses (array of all email addresses should become empty)`, async () => {
+      const emailToAdd1 = 'test1@gmail.com';
+      const emailToAdd2 = 'test2@gmail.com';
+      const emailToAdd3 = 'test3@gmail.com';
+      await db.insertEmail(emailToAdd1);
+      await db.insertEmail(emailToAdd2);
+      await db.insertEmail(emailToAdd3);
+
+      await db.clearDB();
+
+      const emailsArr = await db.findAllEmails();
+      expect(emailsArr).toHaveLength(0);
+    });
+
+    test(`Should not throw an error when trying to clear an empty array`, async () => {
+      try {
+        await db.clearDB();
+
+        const emailsArr = await db.findAllEmails();
+        expect(emailsArr).toHaveLength(0);
+      } catch (err) {
+        expect(err).toBe(undefined);
+      }
+    });
+  });
+});
