be4fab52b3baa68527f684854c2a202b44c05a4b
VasylKhrapko <khrapko2002@gmail.com>
2022-07-26 18:16:56 +0300

    Added dependencies for parsing x-www-form-urlencoded and form-data

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
+const formData = require('express-form-data');
+
 const { PREFIX } = require('./config');
 const rateController = require('./controllers/rateController');
 const subscriptionСontroller = require('./controllers/subscriptionСontroller');
 
 const app = express();
 
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(formData.parse());
 
 app.get(PREFIX + '/rate', rateController.getRate);
 app.post(PREFIX + '/subscribe', subscriptionСontroller.subscribe);
