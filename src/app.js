8b3da93d8e60535d71b6558e92a3c04b72fa3a8a
VasylKhrapko <khrapko2002@gmail.com>
2022-07-26 20:46:03 +0300

    Added validation for subscribe endpoint (incoming email address)

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser');
 const formData = require('express-form-data');
 
 const { PREFIX } = require('./config');
+const validateRequest = require('./middlewares/validateRequest');
+// eslint-disable-next-line max-len
+const subscribeValidationSchema = require('./middlewares/subscribeValidationSchema');
 const rateController = require('./controllers/rateController');
 const subscriptionСontroller = require('./controllers/subscriptionСontroller');
 
@@ -15,6 +18,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(formData.parse());
 
 app.get(PREFIX + '/rate', rateController.getRate);
-app.post(PREFIX + '/subscribe', subscriptionСontroller.subscribe);
+app.post(
+  PREFIX + '/subscribe',
+  subscribeValidationSchema,
+  validateRequest,
+  subscriptionСontroller.subscribe
+);
 
 module.exports = app;
