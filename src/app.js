ea75bfdd00346c390483953020370574ed377cfb
VasylKhrapko <khrapko2002@gmail.com>
2022-07-27 18:39:32 +0300

    Refactored: routes moved to a specialized folder

--- src/app.js
+++ src/app.js
@@ -4,12 +4,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const formData = require('express-form-data');
 
+const router = require('./routes/router');
+
 const { PREFIX } = require('./config');
-const validateRequest = require('./middlewares/validateRequest');
-// eslint-disable-next-line max-len
-const subscribeValidationSchema = require('./middlewares/subscribeValidationSchema');
-const rateController = require('./controllers/rateController');
-const subscriptionСontroller = require('./controllers/subscriptionСontroller');
 
 const app = express();
 
@@ -17,12 +14,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(formData.parse());
 
-app.get(PREFIX + '/rate', rateController.getRate);
-app.post(
-  PREFIX + '/subscribe',
-  subscribeValidationSchema,
-  validateRequest,
-  subscriptionСontroller.subscribe
-);
-app.post(PREFIX + '/sendEmails', subscriptionСontroller.sendEmails);
+app.use(PREFIX, router);
+
 module.exports = app;
